498b80d8233db417f992af7b5507ee4868affa8b
Ryan Apratama <ryanapratama16@gmail.com>
2022-06-30 21:25:11 +0700

    allow cors on crhome

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
+// set the cors conf to disabled
+// chrome://flags/#block-insecure-private-network-requests
+
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
